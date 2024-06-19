3619d8f0d8f029b8249ebc4ec7a3cffe15ea48a9
arelydtr <deyanitamr@gmail.com>
2024-06-18 22:33:28 -0600

    skill

---
+++ lambda/index.js
@@ -0,0 +1,334 @@
+/* *
+ * This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
+ * Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+ * session persistence, api calls, and more.
+ * */
+
+const Alexa = require('ask-sdk-core');
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Hola Arely este es mi Skill';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const SumIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'SumIntent';
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const cantidad1 = handlerInput.requestEnvelope.request.intent.slots.uno.value;
+        const cantidad2 = handlerInput.requestEnvelope.request.intent.slots.dos.value;
+        var uno = Number(cantidad1);
+        var dos = Number(cantidad2);
+        
+        if (isNaN(uno) || isNaN(dos)) {
+            return handlerInput.responseBuilder
+                .speak('Lo siento, no pude entender los números. Por favor, inténtalo de nuevo.')
+                .getResponse();
+        }
+        
+        let resultado = 0;
+        resultado = uno + dos;
+        const speakOutput =  `Calculadora UTH... El resultado de la suma de ${uno} más ${dos} es igual a ${resultado}.`;
+        
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+const ResIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'ResIntent';
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const cantidad1 = handlerInput.requestEnvelope.request.intent.slots.uno.value;
+        const cantidad2 = handlerInput.requestEnvelope.request.intent.slots.dos.value;
+        var uno = Number(cantidad1);
+        var dos = Number(cantidad2);
+        
+        if (isNaN(uno) || isNaN(dos)) {
+            return handlerInput.responseBuilder
+                .speak('Lo siento, no pude entender los números. Por favor, inténtalo de nuevo.')
+                .getResponse();
+        }
+        
+        let resultado = 0;
+        resultado = uno - dos;
+        const speakOutput = `Calculadora UTH... El resultado de la resta de ${uno} menos ${dos} es igual a ${resultado}.`;
+        
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+const MulIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'MulIntent';
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const cantidad1 = handlerInput.requestEnvelope.request.intent.slots.uno.value;
+        const cantidad2 = handlerInput.requestEnvelope.request.intent.slots.dos.value;
+        var uno = Number(cantidad1);
+        var dos = Number(cantidad2);
+        
+        if (isNaN(uno) || isNaN(dos)) {
+            return handlerInput.responseBuilder
+                .speak('Lo siento, no pude entender los números. Por favor, inténtalo de nuevo.')
+                .getResponse();
+        }
+        
+        let resultado = 0;
+        resultado = uno * dos;
+        const speakOutput = `Calculadora UTH... El resultado de la multiplicacion de ${uno} por ${dos} es igual a ${resultado}.`;
+        
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+const DivIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'DivIntent';
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const cantidad1 = handlerInput.requestEnvelope.request.intent.slots.uno.value;
+        const cantidad2 = handlerInput.requestEnvelope.request.intent.slots.dos.value;
+        var uno = Number(cantidad1);
+        var dos = Number(cantidad2);
+        
+        if (isNaN(uno) || isNaN(dos)) {
+            return handlerInput.responseBuilder
+                .speak('Lo siento, no pude entender los números. Por favor, inténtalo de nuevo.')
+                .getResponse();
+        }
+        
+        let resultado = 0;
+        resultado = uno / dos;
+        const speakOutput = `Calculadora UTH... El resultado de la division de ${uno} entre ${dos} es igual a ${resultado}.`;
+        
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+const PotIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'PotIntent';
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const cantidad1 = handlerInput.requestEnvelope.request.intent.slots.uno.value;
+        const cantidad2 = handlerInput.requestEnvelope.request.intent.slots.dos.value;
+        var uno = Number(cantidad1);
+        var dos = Number(cantidad2);
+        
+        if (isNaN(uno) || isNaN(dos)) {
+            return handlerInput.responseBuilder
+                .speak('Lo siento, no pude entender los números. Por favor, inténtalo de nuevo.')
+                .getResponse();
+        }
+        
+        let resultado = 0;
+        resultado = Math.pow(parseInt(uno), parseInt(dos));
+        const speakOutput = `Calculadora UTH... El resultado de ${uno} a la ${dos} es igual a ${resultado}.`;
+        
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+const CosIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+            Alexa.getIntentName(handlerInput.requestEnvelope) === 'CosIntent';
+    },
+    handle(handlerInput) {
+        const uno = handlerInput.requestEnvelope.request.intent.slots.uno.value;
+        const resultado = Math.cos(parseFloat(uno));
+        
+        const speakOutput = `Calculadora UTH... El coseno de ${uno} es ${resultado}.`;
+        
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+
+
+const HelloWorldIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Hello World!';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+
+
+
+
+
+
+
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'You can say hello to me! How can I help?';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Goodbye!';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * FallbackIntent triggers when a customer says something that doesn’t map to any intents in your skill
+ * It must also be defined in the language model (if the locale supports it)
+ * This handler can be safely added but will be ingnored in locales that do not support it yet 
+ * */
+const FallbackIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Sorry, I don\'t know about that. Please try again.';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
+ * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
+ * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
+ * */
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
+    }
+};
+/* *
+ * The intent reflector is used for interaction model testing and debugging.
+ * It will simply repeat the intent the user said. You can create custom handlers for your intents 
+ * by defining them above, then also adding them to the request handler chain below 
+ * */
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `You just triggered ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+/**
+ * Generic error handling to capture any syntax or routing errors. If you receive an error
+ * stating the request handler chain is not found, you have not implemented a handler for
+ * the intent being invoked or included it in the skill builder below 
+ * */
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const speakOutput = 'Sorry, I had trouble doing what you asked. Please try again.';
+        console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+/**
+ * This handler acts as the entry point for your skill, routing all request and response
+ * payloads to the handlers above. Make sure any new handlers or interceptors you've
+ * defined are included below. The order matters - they're processed top to bottom 
+ * */
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        HelloWorldIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        FallbackIntentHandler,
+        SessionEndedRequestHandler,
+        SumIntentHandler,
+        ResIntentHandler,
+        MulIntentHandler,
+        DivIntentHandler,
+        PotIntentHandler,
+        CosIntentHandler,
+        IntentReflectorHandler)
+    .addErrorHandlers(
+        ErrorHandler)
+    .withCustomUserAgent('sample/hello-world/v1.2')
+    .lambda();
